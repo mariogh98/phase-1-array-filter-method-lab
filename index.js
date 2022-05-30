9fe6bb175231f8a487c988ae8fd872c00af5cbbd
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-29 20:47:43 -0500

    commit tree 79557bae5ff1191af0c1a5448a21b7ae8a1405dd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(arr, string) {
+    return arr.filter (function (el) {
+        return el.toLowerCase().indexOf(string.toLowerCase()) !== -1
+    });
+}
+
+
+/*const fuzzyMatch =(arr, string) => {
+    return arr.filter(el => el.toLowerCase().indexOf(string.toLowerCase()) !== -1)
+}*/
+
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(possibleMatch => possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+
+
+function matchName(drivers, string) {
+    return drivers.filter(function(hometown){
+        return hometown.name === string;
+    })
+}
